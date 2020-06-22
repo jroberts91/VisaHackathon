df6cfa37112448c8eea8fb9bc4537bc2881fa619
ongyz <ongyanzhi96@gmail.com>
2020-06-22 15:02:19 +0800

    fix error form merge

--- Web/BackEnd/server.js
+++ Web/BackEnd/server.js
@@ -15,13 +15,6 @@ const connect = mongoose.connect(process.env.MONGO_SECRET,
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 app.use(morgan('combined'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
