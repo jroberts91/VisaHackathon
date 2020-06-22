3079db422f454edec68700c59bd99e1bf04a6e53
Josh Robert <j_.roberts@outlook.com>
2020-06-22 16:30:40 +0800

    First draft offers page

--- Web/FrontEnd/src/Views/MerchantShop/MerchantShop.js
+++ Web/FrontEnd/src/Views/MerchantShop/MerchantShop.js
@@ -12,10 +12,10 @@ const { Title } = Typography;
 
 const StyledIcon = styled(PlusOutlined)`
   min-height: 220px;
-  height: 100%; 
+  height: 100%;
   font-size: 10em;
-  color: black; 
-  background-color: #FAAA13;
+  color: black;
+  background-color: #faaa13;
   line-height: 1;
   vertical-align: middle;
 `;
@@ -26,32 +26,32 @@ class ProductList extends React.Component {
 
     return (
       <Row gutter={[32, 32]}>
-        {
-          isOwnerShop &&
+        {isOwnerShop && (
           <Col key={-1} lg={{ span: 8 }} md={{ span: 12 }} sm={{ span: 24 }} span={24}>
             <Link to={`/${merchantId}/addproduct`}>
-              <Card
-                style={{ width: '100%', minWidth: 250, height: '100%' }}
-                hoverable
-                cover={<StyledIcon />}
-              >
-                <Meta title={"Add New Product"} />
+              <Card style={{ width: '100%', minWidth: 250, height: '100%' }} hoverable cover={<StyledIcon />}>
+                <Meta title={'Add New Product'} />
               </Card>
             </Link>
           </Col>
-        }
+        )}
         {products.map((product, index) => {
           const { name, images, url, rating, _id } = product;
           return (
             <Col key={index} lg={{ span: 8 }} md={{ span: 12 }} sm={{ span: 24 }} span={24}>
-              <ProductCard title={name} imageUrl={images[0]} rating={rating}
-                productUrl={url} productId={_id} merchantId={merchantId}
-                isOwnerShop={isOwnerShop} />
+              <ProductCard
+                title={name}
+                imageUrl={images[0]}
+                rating={rating}
+                productUrl={url}
+                productId={_id}
+                merchantId={merchantId}
+                isOwnerShop={isOwnerShop}
+              />
             </Col>
           );
-        })
-        }
-      </Row >
+        })}
+      </Row>
     );
   }
 }
@@ -62,43 +62,40 @@ export default class MerchantShop extends React.Component {
     this.state = {
       merchantId: this.props.match.params.merchantId,
       products: [],
-      merchantName: "",
-      isOwnerShop: false
-    }
+      merchantName: '',
+      isOwnerShop: false,
+    };
   }
 
   getProductFromApi = (merchantId) => {
     const body = {
-      merchantId: merchantId
-    }
-    API
-      .post('api/product/getAll', body)
+      merchantId: merchantId,
+    };
+    API.post('api/product/getAll', body)
       .then((res) => {
         this.setState({ products: res.data.products });
       })
       .catch((err) => console.error(err));
-  }
+  };
 
   getMerchantNameFromApi = (merchantId) => {
-    API
-      .get('api/merchant/get?id=' + merchantId)
+    API.get('api/merchant/get?id=' + merchantId)
       .then((res) => {
         this.setState({ merchantName: res.data.merchant.name });
       })
       .catch((err) => console.error(err));
-  }
+  };
 
   getIsOwnerShopFromApi = (merchantId) => {
-    API
-      .get('api/merchant/auth').then((res) => {
-        const { success, _id } = res.data;
-        if (success) {
-          this.setState({
-            isOwnerShop: _id === merchantId
-          });
-        }
-      });
-  }
+    API.get('api/merchant/auth').then((res) => {
+      const { success, _id } = res.data;
+      if (success) {
+        this.setState({
+          isOwnerShop: _id === merchantId,
+        });
+      }
+    });
+  };
 
   componentDidMount = () => {
     const merchantId = this.state.merchantId;
@@ -115,7 +112,7 @@ export default class MerchantShop extends React.Component {
       this.getMerchantNameFromApi(newMerchantId);
       this.getIsOwnerShopFromApi(newMerchantId);
     }
-  }
+  };
 
   render() {
     const { merchantId, products, isOwnerShop } = this.state;
@@ -123,7 +120,9 @@ export default class MerchantShop extends React.Component {
     return (
       <Content style={{ maxWidth: '1280px', margin: '0 auto', width: '90%' }}>
         <Row align="top" justify="space-between" style={{ margin: '30px 0 10px 0' }}>
-          <Title level={4} style={{ color: "#828282" }}><HomeOutlined /> / <UserOutlined /> {this.state.merchantName}</Title>
+          <Title level={4} style={{ color: '#828282' }}>
+            <HomeOutlined /> / <UserOutlined /> {this.state.merchantName}
+          </Title>
         </Row>
         <ProductList merchantId={merchantId} products={products} isOwnerShop={isOwnerShop} />
       </Content>
