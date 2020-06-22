f2bf2e8039c521f13477170a118c65b9ec4fc5a2
ongyz <ongyanzhi96@gmail.com>
2020-06-22 16:45:48 +0800

    add VMORC offers list api

--- Web/BackEnd/routes/offers.js
+++ Web/BackEnd/routes/offers.js
@@ -1,17 +1,42 @@
 const express = require('express');
+const https = require('https');
+const axios = require('axios');
+const path = require("path");
+fs = require('fs');
+
 const router = express.Router();
-const { RetrieveAllOffers } = require("../external/vmorc");
+require('dotenv').config();
 
 //=================================
 //             Offers
 //=================================
 
+var config = {
+    method: 'get',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': "Basic " + new Buffer(process.env.VD_USERNAME + ":" + process.env.VD_PASSWORD).toString("base64")
+    },
+    httpsAgent: new https.Agent({
+        cert: fs.readFileSync(path.resolve(__dirname, "../resources/cert.pem")),
+        key: fs.readFileSync(path.resolve(__dirname, "../resources/key.pem")),
+    })
+};
+
 // current implementation only gets all offers with no filter
 //?max=int
 router.get("/list", async (req, res) => {
     let max = req.query.max;
-    var data = await RetrieveAllOffers(max);
-    return res.status(200).json(data);
+    config.url = process.env.VMORC_GET_ALL_URL;
+    config.params = {max_offers: parseInt(max)};
+    axios(config).then(function (response) {
+        var data = response.data;
+        data.success = true;
+        return res.status(200).json(data)
+    }).catch(function (error) {
+        console.log(error);
+        return res.status(400).json({success: false, err})
+    });
 });
 
 module.exports = router;
