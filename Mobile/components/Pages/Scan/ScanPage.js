0591f682707533c693c08a285ff3289657d14784
jamessspanggg <jamespangmunwai@gmail.com>
2020-06-23 17:44:44 +0800

    Use expo barcode scanner instead of camera

--- Mobile/components/Pages/Scan/ScanPage.js
+++ Mobile/components/Pages/Scan/ScanPage.js
@@ -1,47 +1,31 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  Text,
+  TouchableOpacity,
+  LayoutAnimation,
+  Alert,
+  Linking,
+  StatusBar,
+} from 'react-native';
 import { Icon } from 'react-native-elements';
 import * as Permissions from 'expo-permissions';
-import { connect } from 'react-redux';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
-});
+class ScanPage extends Component {
+  state = {
+    hasCameraPermission: null,
+    lastScannedUrl: null,
+    cameraType: BarCodeScanner.Constants.Type.back,
+  };
 
-class ScanPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasPermission: null,
-      type: Camera.Constants.Type.back,
-    };
+  componentDidMount() {
+    this._requestCameraPermission();
   }
 
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasPermission: status === 'granted' });
-  }
-
-  handleCameraType = () => {
+  _handleCameraType = () => {
     const { cameraType } = this.state;
 
     this.setState({
@@ -49,46 +33,114 @@ class ScanPage extends React.Component {
     });
   };
 
-  render() {
-    const { hasPermission } = this.state;
-    if (hasPermission === null) {
-      return <View />;
-    } else if (hasPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={styles.container}>
-          <Camera style={{ flex: 1 }} type={this.state.cameraType}>
-            <View
-              style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                marginBottom: 20,
-                marginLeft: 'auto',
-                marginRight: 'auto',
-              }}
-            >
-              <TouchableOpacity
-                onPress={() => this.handleCameraType()}
-                style={{
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                  backgroundColor: 'transparent',
-                }}
-              >
-                <Icon name="sync-alt" type="font-awesome-5" size={30} color="white" />
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
+  _requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({
+      hasCameraPermission: status === 'granted',
+    });
+  };
+
+  _handleBarCodeRead = (result) => {
+    if (result.data !== this.state.lastScannedUrl) {
+      LayoutAnimation.spring();
+      this.setState({ lastScannedUrl: result.data });
     }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ? (
+          <Text>Requesting for camera permission</Text>
+        ) : this.state.hasCameraPermission === false ? (
+          <Text style={{ color: '#fff' }}>Camera permission is not granted</Text>
+        ) : (
+          <BarCodeScanner
+            onBarCodeScanned={this._handleBarCodeRead}
+            style={{
+              height: Dimensions.get('window').height,
+              width: Dimensions.get('window').width,
+            }}
+          />
+        )}
+
+        {this._maybeRenderUrl()}
+
+        <StatusBar hidden />
+      </View>
+    );
   }
+
+  _handlePressUrl = () => {
+    Alert.alert(
+      'Open this URL?',
+      this.state.lastScannedUrl,
+      [
+        {
+          text: 'Yes',
+          onPress: () => Linking.openURL(this.state.lastScannedUrl),
+        },
+        { text: 'No', onPress: () => {} },
+      ],
+      { cancellable: false }
+    );
+  };
+
+  _handlePressCancel = () => {
+    this.setState({ lastScannedUrl: null });
+  };
+
+  _maybeRenderUrl = () => {
+    if (!this.state.lastScannedUrl) {
+      return;
+    }
+
+    return (
+      <View style={styles.bottomBar}>
+        <TouchableOpacity style={styles.url} onPress={this._handlePressUrl}>
+          <Text numberOfLines={1} style={styles.urlText}>
+            {this.state.lastScannedUrl}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.cancelButton} onPress={this._handlePressCancel}>
+          <Text style={styles.cancelButtonText}>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 }
 
-const mapStateToProps = (state) => ({
-  users: state.users,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  bottomBar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 15,
+    flexDirection: 'row',
+  },
+  url: {
+    flex: 1,
+  },
+  urlText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  cancelButton: {
+    marginLeft: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cancelButtonText: {
+    color: 'rgba(255,255,255,0.8)',
+    fontSize: 18,
+  },
 });
 
-export default connect(mapStateToProps)(ScanPage);
+export default ScanPage;
