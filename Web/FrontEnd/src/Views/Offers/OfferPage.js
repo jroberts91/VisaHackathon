97941ab49a596cb4182cf1c9eb2c8be21e03f21e
Josh Robert <j_.roberts@outlook.com>
2020-06-23 21:57:24 +0800

    Add loading sign for offers page

--- Web/FrontEnd/src/Views/Offers/OfferPage.js
+++ Web/FrontEnd/src/Views/Offers/OfferPage.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Row, Col, Layout, Typography } from 'antd';
+import styled from 'styled-components';
+import { Row, Col, Layout, Typography, Spin } from 'antd';
 import OfferCard from '../../Components/Cards/OfferCard';
 import API from '../../utils/baseUrl';
 
 const { Content } = Layout;
 const { Title } = Typography;
 
+const StyledSpin = styled(Spin)`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+`;
+
 export default class OfferPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { offers: [] };
+    this.state = { offers: [], notLoaded: true };
   }
 
   componentDidMount = () => {
@@ -17,18 +24,22 @@ export default class OfferPage extends React.Component {
   };
 
   getOffersFromApi = () => {
+    this.setState({ notLoaded: true });
     API.get('api/offers/list?max=' + '10')
       .then((res) => {
         console.log(res.data.Offers);
+        this.setState({ notLoaded: false });
         this.setState({ offers: res.data.Offers });
       })
       .catch((err) => console.error(err));
   };
 
   render() {
-    const { offers } = this.state;
+    const { offers, notLoaded } = this.state;
+
     return (
       <Content style={{ maxWidth: '1280px', margin: '0 auto', width: '90%' }}>
+        {notLoaded && <StyledSpin size="large" />}
         <Row align="top" justify="space-between" style={{ margin: '30px 0 10px 0' }}>
           <Title level={4} style={{ color: '#828282' }}>
             Offers
