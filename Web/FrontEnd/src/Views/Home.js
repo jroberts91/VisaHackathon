67d1a45cd842fed6ef62c581c4cbbdd85456ed20
jamessspanggg <jamespangmunwai@gmail.com>
2020-06-25 21:15:33 +0800

    Fix issue with invalid user id message

--- Web/FrontEnd/src/Views/Home.js
+++ Web/FrontEnd/src/Views/Home.js
@@ -59,11 +59,11 @@ export default class Home extends React.Component {
   };
 
   render() {
-    const { collapsed, isLoggedIn, username, merchantId, isMounted } = this.state;
-    const toggleSideDrawer = () => this.setState({ collapsed: !collapsed });
-    if (!isMounted) {
+    if (!this.state.isMounted) {
       return null; // only return the content when user is finished authenticating
     }
+    const { collapsed, isLoggedIn, username, merchantId } = this.state;
+    const toggleSideDrawer = () => this.setState({ collapsed: !collapsed });
     return (
       <Layout>
         <SideBar collapsed={collapsed} isLoggedIn={isLoggedIn} merchantId={merchantId} />
