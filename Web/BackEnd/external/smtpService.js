e049a6f7846cd5dcbfe134fdbd666d449f27d180
ongyz <ongyanzhi96@gmail.com>
2020-06-26 17:03:43 +0800

    add mobile payment

--- Web/BackEnd/external/smtpService.js
+++ Web/BackEnd/external/smtpService.js
@@ -4,73 +4,68 @@ const nodemailer = require('nodemailer');
 
 require('dotenv').config();
 
-var readHTMLFile = function(path, callback) {
-    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
-        if (err) {
-            throw err;
-            callback(err);
-        }
-        else {
-            callback(null, html);
-        }
-    });
+var readHTMLFile = function (path, callback) {
+  fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+    if (err) {
+      throw err;
+      callback(err);
+    } else {
+      callback(null, html);
+    }
+  });
 };
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: process.env.GMAIL_USER_NAME,
-           pass: process.env.GMAIL_USER_PASSWORD
-       }
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER_NAME,
+    pass: process.env.GMAIL_USER_PASSWORD,
+  },
 });
 
 let SendPaymentEmail = async (email) => {
-    // readHTMLFile(__dirname + 'resources/payment.html', function(err, html) {
-    //     var template = handlebars.compile(html);
-    //     var replacements = {
-    //          username: "John Doe"
-    //     };
-    //     var htmlToSend = template(replacements);
-    //     const mailOptions = {
-    //         from: process.env.GMAIL_USER_NAME, // sender address
-    //         to: email, // list of receivers
-    //         subject: 'ViSell - We have received your payment!', // Subject line
-    //         html: htmlToSend// plain text body
-    //     };
-    //     smtpTransport.sendMail(mailOptions, function (error, response) {
-    //         if (error) {
-    //             console.log(error);
-    //             callback(error);
-    //         }
-    //     });
-    // });
-    const mailOptions = {
-        from: process.env.GMAIL_USER_NAME, // sender address
-        to: email, // list of receivers
-        subject: 'ViSell - We have received your payment!', // Subject line
-        html: 'You should receive a confirmation email once your order has been fulfilled.'// plain text body
-    };
-    transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
-        console.log(err)
-    else
-        console.log(info);
-    });
+  // readHTMLFile(__dirname + 'resources/payment.html', function(err, html) {
+  //     var template = handlebars.compile(html);
+  //     var replacements = {
+  //          username: "John Doe"
+  //     };
+  //     var htmlToSend = template(replacements);
+  //     const mailOptions = {
+  //         from: process.env.GMAIL_USER_NAME, // sender address
+  //         to: email, // list of receivers
+  //         subject: 'ViSell - We have received your payment!', // Subject line
+  //         html: htmlToSend// plain text body
+  //     };
+  //     smtpTransport.sendMail(mailOptions, function (error, response) {
+  //         if (error) {
+  //             console.log(error);
+  //             callback(error);
+  //         }
+  //     });
+  // });
+  const mailOptions = {
+    from: process.env.GMAIL_USER_NAME, // sender address
+    to: email, // list of receivers
+    subject: 'ViSell - We have received your payment!', // Subject line
+    html: 'You should receive a confirmation email once your order has been fulfilled.', // plain text body
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
 };
 
 let SendFulfillEmail = async (email) => {
-    const mailOptions = {
-        from: process.env.GMAIL_USER_NAME, // sender address
-        to: email, // list of receivers
-        subject: 'ViSell - Your Order has been Fulfilled!', // Subject line
-        html: 'Your order has been confirmed.'// plain text body
-    };
-    transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
-        console.log(err)
-    else
-        console.log(info);
-    });
+  const mailOptions = {
+    from: process.env.GMAIL_USER_NAME, // sender address
+    to: email, // list of receivers
+    subject: 'ViSell - Your Order has been Fulfilled!', // Subject line
+    html: 'Your order has been confirmed.', // plain text body
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
 };
 
-module.exports = { SendPaymentEmail, SendFulfillEmail }
+module.exports = { SendPaymentEmail, SendFulfillEmail };
--- Web/BackEnd/routes/offers.js
+++ Web/BackEnd/routes/offers.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const https = require('https');
 const axios = require('axios');
-const path = require("path");
+const path = require('path');
 fs = require('fs');
 
 const router = express.Router();
@@ -12,30 +12,32 @@ require('dotenv').config();
 //=================================
 
 var config = {
-    method: 'get',
-    headers: { 
-      'Content-Type': 'application/json', 
-      'Authorization': "Basic " + new Buffer(process.env.VD_USERNAME + ":" + process.env.VD_PASSWORD).toString("base64")
-    },
-    httpsAgent: new https.Agent({
-        cert: fs.readFileSync(path.resolve(__dirname, "../resources/cert.pem")),
-        key: fs.readFileSync(path.resolve(__dirname, "../resources/key.pem")),
-    })
+  method: 'get',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Basic ' + new Buffer(process.env.VD_USERNAME + ':' + process.env.VD_PASSWORD).toString('base64'),
+  },
+  httpsAgent: new https.Agent({
+    cert: fs.readFileSync(path.resolve(__dirname, '../resources/cert.pem')),
+    key: fs.readFileSync(path.resolve(__dirname, '../resources/key.pem')),
+  }),
 };
 
 // current implementation only gets all offers with no filter
 //?max=int
-router.get("/list", async (req, res) => {
-    let max = req.query.max;
-    config.url = process.env.VMORC_GET_ALL_URL;
-    config.params = {max_offers: parseInt(max)};
-    axios(config).then(function (response) {
-        var data = response.data;
-        data.success = true;
-        return res.json(data)
-    }).catch(function (error) {
-        console.log(error);
-        return res.json({success: false, err})
+router.get('/list', async (req, res) => {
+  let max = req.query.max;
+  config.url = process.env.VMORC_GET_ALL_URL;
+  config.params = { max_offers: parseInt(max) };
+  axios(config)
+    .then(function (response) {
+      var data = response.data;
+      data.success = true;
+      return res.json(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return res.json({ success: false, err });
     });
 });
 
