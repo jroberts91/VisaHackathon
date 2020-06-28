382348e646cba6760ff862b7c896f7468c227e1a
ongyz <ongyanzhi96@gmail.com>
2020-06-28 18:02:59 +0800

    add merchantimage to offer

--- Web/BackEnd/external/smtpService.js
+++ Web/BackEnd/external/smtpService.js
@@ -4,71 +4,66 @@ const fs = require('fs');
 
 require('dotenv').config();
 
-var readHTMLFile = function(path, callback) {
-    fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
-        if (err) {
-            throw err;
-            callback(err);
-        }
-        else {
-            callback(null, html);
-        }
-    });
+var readHTMLFile = function (path, callback) {
+  fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+    if (err) {
+      throw err;
+      callback(err);
+    } else {
+      callback(null, html);
+    }
+  });
 };
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: process.env.GMAIL_USER_NAME,
-           pass: process.env.GMAIL_USER_PASSWORD
-       }
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER_NAME,
+    pass: process.env.GMAIL_USER_PASSWORD,
+  },
 });
 
 let SendPaymentEmail = async (email, orderId, merchantName, merchantEmail) => {
-    readHTMLFile(__dirname + '/template/payment-email-template.html', function(err, html) {
-        var template = handlebars.compile(html);
-        var replacements = {
-            orderSummaryLink: process.env.WEB_HOST + "/order/" + orderId,
-            merchant: merchantName,
-            merchantEmail: merchantEmail
-        };
-        var htmlToSend = template(replacements);
-        const mailOptions = {
-            from: process.env.GMAIL_USER_NAME, // sender address
-            to: email, // list of receivers
-            subject: 'ViSell - We have received your payment!', // Subject line
-            html: htmlToSend// plain text body
-        };
-        transporter.sendMail(mailOptions, function (err, info) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info);
-        });
+  readHTMLFile(__dirname + '/template/payment-email-template.html', function (err, html) {
+    var template = handlebars.compile(html);
+    var replacements = {
+      orderSummaryLink: process.env.WEB_HOST + '/order/' + orderId,
+      merchant: merchantName,
+      merchantEmail: merchantEmail,
+    };
+    var htmlToSend = template(replacements);
+    const mailOptions = {
+      from: process.env.GMAIL_USER_NAME, // sender address
+      to: email, // list of receivers
+      subject: 'ViSell - We have received your payment!', // Subject line
+      html: htmlToSend, // plain text body
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) console.log(err);
+      else console.log(info);
     });
+  });
 };
 
 let SendFulfillEmail = async (email, orderId) => {
-    readHTMLFile(__dirname + '/template/fulfill-email-template.html', function(err, html) {
-        var template = handlebars.compile(html);
-        var replacements = {
-            orderSummaryLink: process.env.WEB_HOST + "/order/" + orderId,
-            contactUsLink:  process.env.WEB_HOST + "/contactUs",
-        };
-        var htmlToSend = template(replacements);
-        const mailOptions = {
-            from: process.env.GMAIL_USER_NAME, // sender address
-            to: email, // list of receivers
-            subject: 'ViSell - Your Order has been Fulfilled!', // Subject line
-            html: htmlToSend// plain text body
-        };
-        transporter.sendMail(mailOptions, function (err, info) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info);
-        });
+  readHTMLFile(__dirname + '/template/fulfill-email-template.html', function (err, html) {
+    var template = handlebars.compile(html);
+    var replacements = {
+      orderSummaryLink: process.env.WEB_HOST + '/order/' + orderId,
+      contactUsLink: process.env.WEB_HOST + '/contactUs',
+    };
+    var htmlToSend = template(replacements);
+    const mailOptions = {
+      from: process.env.GMAIL_USER_NAME, // sender address
+      to: email, // list of receivers
+      subject: 'ViSell - Your Order has been Fulfilled!', // Subject line
+      html: htmlToSend, // plain text body
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) console.log(err);
+      else console.log(info);
     });
+  });
 };
 
-module.exports = { SendPaymentEmail, SendFulfillEmail }
+module.exports = { SendPaymentEmail, SendFulfillEmail };
--- Web/BackEnd/routes/payment.js
+++ Web/BackEnd/routes/payment.js
@@ -11,64 +11,64 @@ const { SendPaymentEmail } = require('../external/smtpService');
 //             Payment
 //=================================
 
-router.post("/direct", async (req, res) => {
-    // validate order info and create payment 
-    var orderBody = req.body.order;
-    var paymentBody = req.body.payment;
+router.post('/direct', async (req, res) => {
+  // validate order info and create payment
+  var orderBody = req.body.order;
+  var paymentBody = req.body.payment;
 
-    let merchantId = orderBody.merchantId;
-    const merchant = await Merchant.findOne({"_id": merchantId })
-    if (!merchant) return res.json({success: false})
+  let merchantId = orderBody.merchantId;
+  const merchant = await Merchant.findOne({ _id: merchantId });
+  if (!merchant) return res.json({ success: false });
 
-    let productId = orderBody.product;
-    const product = await Product.findOne({"_id": productId})
-    if (!product) return res.json({success: false})
+  let productId = orderBody.product;
+  const product = await Product.findOne({ _id: productId });
+  if (!product) return res.json({ success: false });
 
-    // check product belong to merchant
-    if (product.merchantId != merchantId ) {
-        console.log("merchantId does not match under product");
-        return res.json({success: false});
-    }
-    if (orderBody.quantity < 0) {
-        console.log("product qty cannot be zero");
-        return res.json({success: false});
-    }
-    // check if there is qty available
-    let newSoldQty = parseInt(orderBody.quantity) + product.soldQty;
-    if (newSoldQty > product.totalQty) {
-        console.log("product not available");
-        return res.json({success: false});
-    }
+  // check product belong to merchant
+  if (product.merchantId != merchantId) {
+    console.log('merchantId does not match under product');
+    return res.json({ success: false });
+  }
+  if (orderBody.quantity < 0) {
+    console.log('product qty cannot be zero');
+    return res.json({ success: false });
+  }
+  // check if there is qty available
+  let newSoldQty = parseInt(orderBody.quantity) + product.soldQty;
+  if (newSoldQty > product.totalQty) {
+    console.log('product not available');
+    return res.json({ success: false });
+  }
 
-    paymentBody.dateTime = Date.now(); // set date
-    const payment = new Payment(paymentBody);
-    // order is created after payment is saved
-    orderBody.product = product;
-    const order = new Order(orderBody);
-    try {
-        // pull from consumer
-        await PullFundsTransaction();
-        // push to merchant
-        await PushFundsTransaction();
-        // save only if succesful
-    } catch (err) {
-        console.log(err);
-        return res.json({success: false, err})
-    }
+  paymentBody.dateTime = Date.now(); // set date
+  const payment = new Payment(paymentBody);
+  // order is created after payment is saved
+  orderBody.product = product;
+  const order = new Order(orderBody);
+  try {
+    // pull from consumer
+    await PullFundsTransaction();
+    // push to merchant
+    await PushFundsTransaction();
+    // save only if succesful
+  } catch (err) {
+    console.log(err);
+    return res.json({ success: false, err });
+  }
 
-    // update product qty
-    await Product.findOneAndUpdate({ _id: productId }, { soldQty: newSoldQty });
+  // update product qty
+  await Product.findOneAndUpdate({ _id: productId }, { soldQty: newSoldQty });
 
-    await payment.save(async function(err, payment) {
-        if (err) return res.json({ success: false, err })
-        order.payment = payment;
-        order.isFulfilled = false;
-        await order.save(async function(err, order) {
-            if (err) return res.json({ success: false, err })
-            await SendPaymentEmail(order.email, order._id, merchant.name, merchant.email);
-            return res.json({success: true, orderId: order._id, paymentId: order.payment._id})
-        });
+  await payment.save(async function (err, payment) {
+    if (err) return res.json({ success: false, err });
+    order.payment = payment;
+    order.isFulfilled = false;
+    await order.save(async function (err, order) {
+      if (err) return res.json({ success: false, err });
+      await SendPaymentEmail(order.email, order._id, merchant.name, merchant.email);
+      return res.json({ success: true, orderId: order._id, paymentId: order.payment._id });
     });
+  });
 });
 
 module.exports = router;
