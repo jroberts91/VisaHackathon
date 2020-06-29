97153c6a019534306cab9fe4ea1421aca043171a
Josh Robert <j_.roberts@outlook.com>
2020-06-29 17:40:47 +0800

    Update offer card images

--- Web/FrontEnd/src/Components/Cards/OfferCard.js
+++ Web/FrontEnd/src/Components/Cards/OfferCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card } from 'antd';
 import styled from 'styled-components';
 import 'antd/dist/antd.css';
-import { baseUrl } from '../../utils/baseUrl';
+import API, { baseUrl } from '../../utils/baseUrl';
 import { Link } from 'react-router-dom';
 
 const { Meta } = Card;
@@ -23,15 +23,36 @@ class OfferDescription extends React.Component {
 }
 
 export default class OfferCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: '' };
+  }
+
+  componentDidMount = () => {
+    this.getMerchant();
+  };
+
+  getMerchant = () => {
+    API.get(`api/merchant/get?id=${this.props.id}`)
+      .then((res) => {
+        if (res.data.success) {
+          this.setState({ url: res.data.merchant.profileImage });
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   render() {
     const { title, description, code, imgUrl, id } = this.props;
+
+    const fullImageUrl = imgUrl ? imgUrl : baseUrl + this.state.url;
 
     const merchantHref = `/${id}`;
 
     return (
       <Link to={merchantHref}>
         <StyledCard
-          cover={<img style={{ height: '110px', objectFit: 'cover' }} alt="example" src={imgUrl} />}
+          cover={<img style={{ height: '110px', objectFit: 'cover' }} alt="example" src={fullImageUrl} />}
           hoverable
         >
           <Meta title={title} description={<OfferDescription code={code} description={description} />} />
