bd5665a81836ddcf88794be1be13bde46b845c5d
yidaaa <59677662+yidaaa@users.noreply.github.com>
2020-06-29 16:59:07 +0800

    Update ContactUs.js
    validate messages added
    thank you message added upon submission
    redirected to homepage/dashboard after submission

--- Web/FrontEnd/src/Views/ContactUs/ContactUs.js
+++ Web/FrontEnd/src/Views/ContactUs/ContactUs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Form, Input, Button, Row, Col, Layout, Typography, Select } from 'antd';
+import { Form, Input, Button, Row, Col, Layout, Typography, Select, message } from 'antd';
 import { MessageOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import image from '../../images/feedback.png';
@@ -9,6 +9,10 @@ const { TextArea } = Input;
 const { Content } = Layout;
 const { Title } = Typography;
 const { Option } = Select;
+const validateMessages = {
+  required: 'This field is required!',
+};
+
 
 const StyledImage = styled.img`
   position: relative;
@@ -30,6 +34,13 @@ export default class AddProduct extends React.Component {
       wrapperCol: { span: 24 },
     };
 
+    const handleSubmit = (info) => {
+      message.success({ content: "Thank you for your feedback!"});
+      this.props.history.push({
+        pathname: `/`,
+      });
+    }
+
     return (
       <Content style={{ maxWidth: '1280px', width: '90%', margin: '0 auto' }}>
         <div style={{ margin: '0 auto', width: '100%' }}>
@@ -39,31 +50,31 @@ export default class AddProduct extends React.Component {
             </Title>
           </Row>
 
-          <Form {...layout} ref={this.formRef}>
+          <Form {...layout} ref={this.formRef} validateMessages={validateMessages} onFinish={handleSubmit}>
             <Row align="top">
               <Col lg={{ span: 12 }} span={24}>
                 <Title level={4}> Any questions or feedback? </Title>
 
-                <Form.Item label="I am a" rules={{ required: true }}>
+                <Form.Item label="I am a" name={['identity']} rules={[{ required: true }]}>
                   <Select allowClear style={{ width: '40%' }}>
                     <Option value="buyer">Buyer</Option>
                     <Option value="seller">Seller</Option>
                   </Select>
                 </Form.Item>
 
-                <Form.Item label="Order ID">
+                <Form.Item label="Order ID" name={['order']} rules={[{ required: true }]}>
                   <Input />
                 </Form.Item>
 
-                <Form.Item label="Full Name">
+                <Form.Item label="Full Name" name={['name']} rules={[{ required: true }]}>
                   <Input />
                 </Form.Item>
 
-                <Form.Item label="Email Address" rules={[{ type: 'email' }]}>
+                <Form.Item label="Email Address" name={['email']} rules={[{ type: 'email', required: true }]}>
                   <Input />
                 </Form.Item>
 
-                <Form.Item label="Message">
+                <Form.Item label="Message" name={['message']} rules={[{ required: true }]}>
                   <TextArea autoSize={{ minRows: 6, maxRows: 10 }} style={{ width: '100%' }} />
                 </Form.Item>
 
